1e81a07a15f8152b795f5eae33dc2937fda12750
Andrei Zhlobich <a_zhlobich@wargaming.net>
2013-05-22 18:26:27 +0300

    Initial commit

---
+++ config.fish
@@ -0,0 +1,28 @@
+# Useful functions {{{
+
+function psg -d "Grep for a running process, returning its PID and full string"
+    ps auxww | grep -i --color=always $argv | grep -v grep | collapse | cuts -f 2,11-
+end
+
+function prepend_to_path -d "Prepend the given dir to PATH if it exists and is not already in it"
+    if test -d $argv[1]
+        if not contains $argv[1] $PATH
+            set -gx PATH "$argv[1]" $PATH
+        end
+    end
+end
+
+prepend_to_path "$HOME/bin"
+prepend_to_path "$HOME/.local/bin"
+
+set -g -x fish_greeting ''
+set -g -x EDITOR 'emacs -nw'
+
+function ..; cd ..; end
+function ...; cd ../..; end
+function ....; cd ../../..; end
+function .....; cd ../../../..; end
+
+. ~/.config/fish/virtualfish/virtual.fish
+
+true
---
+++ functions/fish_prompt.fish
@@ -0,0 +1,57 @@
+function __fish_git_in_working_tree
+  [ "true" = (git rev-parse --is-inside-work-tree ^ /dev/null; or echo false) ]
+end
+
+function __fish_git_dirty
+  not git diff --no-ext-diff --quiet --exit-code ^ /dev/null
+  or not git diff-index --cached --quiet HEAD ^ /dev/null
+  or count (git ls-files --others --exclude-standard) > /dev/null
+end
+
+function __fish_git_current_head
+  git symbolic-ref HEAD ^ /dev/null
+  or git describe --contains --all HEAD
+end
+
+function __fish_git_current_branch
+  __fish_git_current_head | sed -e "s#^refs/heads/##"
+end
+
+
+function fish_prompt --description 'Write out the prompt'
+
+    set -l last_status $status
+
+    if [ -n "$VIRTUAL_ENV" ]
+        set_color magenta
+        printf 'vf:%s ' (basename "$VIRTUAL_ENV")
+    end
+
+    set_color $fish_color_cwd
+    printf '%s' (prompt_pwd)
+    
+    if __fish_git_in_working_tree
+        if __fish_git_dirty
+            set_color red
+        else
+            set_color blue
+        end
+        printf " git:%s%s" (__fish_git_current_branch) (set_color normal)
+    end
+
+    if test $last_status -eq 0
+        set_color white -o
+    else
+        set_color red -o
+    end
+
+    switch $USER
+        case root
+	    printf ' # '
+	case '*'
+	    printf ' > '
+    end
+
+    set_color normal
+
+end

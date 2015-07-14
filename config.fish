47c4a365617552181b8f27bf9fd92fe5382d0725
Andrei Zhlobich <a.zhlobich@gmail.com>
2015-07-14 09:42:24 +0300

    Refactor

--- config.fish
+++ config.fish
@@ -1,29 +1,35 @@
 # Useful functions {{{
 
+function ..
+	cd ..
+end
+
+function ...
+	cd ../..
+end
+
 function _prepend_to_path -d "Prepend the given dir to PATH if it exists and is not already in it"
-    if test -d $argv[1]
-        if not contains $argv[1] $PATH
-            set -gx PATH "$argv[1]" $PATH
-        end
-    end
+	if test -d $argv[1]
+		if not contains $argv[1] $PATH
+			set -gx PATH "$argv[1]" $PATH
+		end
+	end
 end
 
 _prepend_to_path $HOME/bin
 _prepend_to_path $HOME/.local/bin
 for p in $HOME/.bins/*
-    _prepend_to_path $p
+	_prepend_to_path $p
 end
 
-set -g -x fish_greeting ''
-set -g -x EDITOR "emacsclient -a '' -nw"
+set -x fish_greeting ''
 
-function ..; cd ..; end
-function ...; cd ../..; end
-function ....; cd ../../..; end
-function .....; cd ../../../..; end
+if type emacsclient >/dev/null
+	set -x EDITOR "emacsclient -a '' -t"
+end
 
 if test -e ~/.fishrc
-   . ~/.fishrc
+	. ~/.fishrc
 end
 
 . ~/.config/fish/virtualfish/virtual.fish
